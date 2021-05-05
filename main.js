ce4f2cb49eb41e78ccee03aa234c37d57af28bf4
Skyler Espinoza <Skyler.Espinoza@sckans.edu>
2021-05-05 11:04:32 -0500

    finalGame

--- main.js
+++ main.js
@@ -7,26 +7,26 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
     const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('canvas')});
     renderer.shadowMap.enabled = true;
     renderer.setSize(renderer.domElement.clientWidth, renderer.domElement.clientHeight);
-    const camera = new THREE.PerspectiveCamera(75, renderer.domElement.clientWidth / renderer.domElement.clientHeight, 0.1, 1000); 
+    const camera = new THREE.PerspectiveCamera(75, renderer.domElement.clientWidth / renderer.domElement.clientHeight, 0.1, 50000); 
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x88CCFF);
-    scene.fog = new THREE.FogExp2(scene.background, 0.1);
+    scene.background = new THREE.Color(0x7700EE);
+    scene.fog = new THREE.FogExp2(scene.background, 0.04);
     
     
 
     // LIGHTING
-    const directional_light = new THREE.DirectionalLight(0xFFFFFF, 1);
+    const directional_light = new THREE.DirectionalLight(0x222222, 1);
     directional_light.position.set(3,4,5);
     directional_light.castShadow = true;
     scene.add(directional_light);
-    const hemi_light = new THREE.HemisphereLight(0xFFFFFF, 0.5);
+    const hemi_light = new THREE.HemisphereLight(0x000000, 0.5);
     scene.add(hemi_light);
 
 
     // LOAD MODEL
     let player, face;
     const loader = new GLTFLoader();
-    loader.load('./assets/MushroomMash.glb', gltf => {
+    loader.load('./assets/MushroomMash2.glb', gltf => {
         scene.add(gltf.scene);
         scene.traverse( node => {
             if(node instanceof THREE.Mesh){
@@ -35,12 +35,16 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
             }
         });
         player = scene.getObjectByName('player');
-        face =  scene.getObjectByName('face');
+        face =  scene.getObjectByName('hat');
         face.add(camera);
-        camera.position.y = 1;
-        camera.position.z = 1;
+        camera.position.y = 3;
+        camera.position.z = 0;
         requestAnimationFrame(animation);
         
+    },undefined, function ( error ) {
+
+        console.error( error );
+    
     });
 
     // KEYBOARD
@@ -63,6 +67,7 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
         }
     });
 
+
     // POINTER
     renderer.domElement.addEventListener('click', click => {
         renderer.domElement.requestPointerLock();
@@ -70,7 +75,7 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
     renderer.domElement.addEventListener('mousemove', mousemove => {
         if(document.pointerLockElement === renderer.domElement) {
             const HEAD_ROTATION_SPEED = 1/256;
-            eye.rotation.x -= mousemove.movementY * HEAD_ROTATION_SPEED;
+            face.rotation.x -= mousemove.movementY * HEAD_ROTATION_SPEED;
             player.rotation.y -= mousemove.movementX * HEAD_ROTATION_SPEED;
         }
     });
@@ -79,11 +84,15 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
     const animation = timestamp => {
 
         // MOVEMENT
-        const movement = new THREE.Vector3(keyboard.d - keyboard.a, 0, keyboard.s - keyboard.w);
+        const speed = 10;
+        const movement = new THREE.Vector3( speed * (keyboard.d - keyboard.a), 0, speed * (keyboard.s - keyboard.w));
         movement.normalize();
         movement.multiplyScalar(1 / 8);
         movement.applyAxisAngle(new THREE.Vector3(0, 1, 1), player.rotation.y);
         player.position.add(movement);
+
+        // RAYCASTING
+
 
 
         // RENDER
